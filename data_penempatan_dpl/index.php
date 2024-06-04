07a575379108d91c1fd76ff98733b0e196552367
nVITTOadrianto <nvittoa@live.com>
2024-06-04 21:56:24 +0700

    Data Penempatan DPL (belum ada CRUD)

---
+++ data_penempatan_dpl/index.php
@@ -0,0 +1,25 @@
+<?php
+    include "../config.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Penempatan DPL - SPKKN</title>
+</head>
+<body>
+    <div class="header">
+        <h1>Sistem Penempatan KKN (SPKKN)</h1>
+    </div>
+    <nav class="navbar">
+        <a href="../">Home</a>
+        <a href="../data_dpl/">Dosen Pembimbing Lapangan</a>
+        <a href="../data_mhs/">Mahasiswa</a>
+        <a href="../data_tempat/">Tempat KKN</a>
+        <a href="#">Penempatan DPL</a>
+        <a href="../data_penempatan_mhs/">Penempatan Mahasiswa</a>
+    </nav>
+    
+</body>
+</html>
