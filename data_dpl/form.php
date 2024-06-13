84f33e6de15036c8494f834de04f238586f883c3
MuhammadFebriArdiyanSaputra <m.febri123.cc@gmail.com>
2024-06-14 00:48:19 +0700

    update head & footer

--- data_dpl/form.php
+++ data_dpl/form.php
@@ -23,12 +23,114 @@ if (isset($_GET['edit'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" style="text/css">
     <title>Form DPL - SPKKN</title>
 </head>
+
+<style>
+    body {
+  font-family: Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+  background-color: #f4f4f4;
+}
+
+.container {
+  width: 100%;
+  margin: auto;
+  overflow: hidden;
+}
+
+.header {
+    background-color: rgba(76, 175, 80, 0.8);
+    color: white;
+    padding: 20px;
+    text-align: left;
+    display: flex;
+    align-items: center;
+}
+
+.header img {
+    margin-right: 20px;
+    height: 50px;
+}
+
+.navbar {
+  display: flex;
+  justify-content: flex-end;
+  background-color: rgba(51, 51, 51, 0.8);
+}
+
+.navbar a {
+  color: white;
+  padding: 14px 20px;
+  text-decoration: none;
+  text-align: center;
+}
+
+.navbar a:hover {
+  background: #575757;
+}
+
+.mt-4 {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+}
+
+.table {
+  width: 80%;
+  border-collapse: collapse;
+  margin: 20px 0;
+}
+
+.table th,
+.table td {
+  padding: 12px;
+  border: 1px solid #ddd;
+  text-align: left;
+}
+
+.table th {
+  background-color: rgba(76, 175, 80, 0.8);
+  color: white;
+}
+
+.btn {
+  display: inline-block;
+  padding: 8px 12px;
+  color: white;
+  text-decoration: none;
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+
+.btn-primary {
+  align-self: baseline;
+  margin-left: 150px;
+  background-color: rgba(76, 175, 80, 0.8);
+}
+
+.btn-info {
+  background: #5bc0de;
+}
+
+.btn-danger {
+  background: #d9534f;
+}
+
+.btn:hover {
+  opacity: 0.8;
+}
+
+</style>
+
 <body>
     <div class="container">
         <div class="header">
+            <img src="../logo.png">
             <h1>Sistem Penempatan KKN (SPKKN)</h1>
         </div>
         <nav class="navbar">
